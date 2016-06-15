b08fc9e75e2f97db54767eda093614335b8db914
Larissa Cardoso <LarissaRattigueri@Larissas-MacBook-Air.local>
2016-06-15 09:26:57 -0700

    Add some style

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,6 +10,7 @@ class EmployeesController < ApplicationController
   # GET /employees/1
   # GET /employees/1.json
   def show
+    @employee = Employee.find(params[:id])
   end
 
   # GET /employees/new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def formatDate(date, horario)
+    if horario
+      date.strftime("%b-%d-%Y %I:%M%p")
+    else
+      date.strftime("%b-%d-%Y")
+    end
+  end
 end
